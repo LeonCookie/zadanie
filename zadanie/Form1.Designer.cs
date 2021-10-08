215cd83bc41cdbe7e5cae7270f900c37bed60291
LeonCookie <leongdynia1105@gmail.com>
2021-10-08 09:34:47 +0200

    Zmiany 1

--- zadanie/Form1.Designer.cs
+++ zadanie/Form1.Designer.cs
@@ -29,20 +29,77 @@ namespace zadanie
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxImie = new System.Windows.Forms.TextBox();
+            this.textBoxNazwaBadania = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
+            // 
+            // textBoxImie
+            // 
+            this.textBoxImie.Location = new System.Drawing.Point(40, 30);
+            this.textBoxImie.Name = "textBoxImie";
+            this.textBoxImie.Size = new System.Drawing.Size(100, 20);
+            this.textBoxImie.TabIndex = 0;
+            this.textBoxImie.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxNazwaBadania
+            // 
+            this.textBoxNazwaBadania.Location = new System.Drawing.Point(40, 78);
+            this.textBoxNazwaBadania.Name = "textBoxNazwaBadania";
+            this.textBoxNazwaBadania.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNazwaBadania.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "imie";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "nazwa badania";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(40, 117);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNazwaBadania);
+            this.Controls.Add(this.textBoxImie);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxImie;
+        private System.Windows.Forms.TextBox textBoxNazwaBadania;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- zadanie/Form1.cs
+++ zadanie/Form1.cs
@@ -16,5 +16,10 @@ namespace zadanie
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
