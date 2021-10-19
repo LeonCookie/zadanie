746724f4c2eedda340e724cbbcb6ec37e5aaa2c7
LeonCookie <leongdynia1105@gmail.com>
2021-10-19 19:00:13 +0200

    button to  txt zapisuje plik na dysku

--- zadanie/Form1.cs
+++ zadanie/Form1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Windows.Forms;
+using System.IO;
+
 
 
 
@@ -8,15 +10,16 @@ namespace zadanie
 {
     public partial class Form1 : Form
     {
-        
-       
 
+
+        //zmienne
+        const int MAXINT = -2147483647;
 
 
         /*string lname, lnBadania;
         string ldata;
         */ // do zabawy z button wyślij
-        
+
 
 
 
@@ -63,6 +66,7 @@ namespace zadanie
         }
         struct pacjenci
         {
+            
             string imie;
             string nazwa_bad;
             float data_bad;
@@ -89,9 +93,28 @@ namespace zadanie
 
 
         }
-            private void buttontxt_Click(object sender, EventArgs e)
+        private void buttontxt_Click(object sender, EventArgs e)
         {
-            //queue simple export file = "queues.txt";
+           
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("E:\\Test.txt");
+                //Write a line of text
+                sw.WriteLine("Hello World!!");
+                //Write a second line of text
+                sw.WriteLine("From the StreamWriter class");
+            
+                //Close the file
+                sw.Close();
+                MessageBox.Show("sprawdz swoj dysk E!");
+
+
+
+
+                System.Diagnostics.Debug.WriteLine("Exception: " );
+            
+            
+                System.Diagnostics.Debug.WriteLine("Executing finally block.");
+            
         }
 
         private void buttonusun_Click(object sender, EventArgs e)
